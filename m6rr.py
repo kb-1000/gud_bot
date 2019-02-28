1860ddb68591d3b30e96e65fd2828968e05293de
Kaeptm Blaubaer <kaeptmblaubaer1000@gmail.com>
2019-02-28 07:47:07 +0100

    Fix a few bot issues

--- m6rr.py
+++ m6rr.py
@@ -12,24 +12,29 @@ class M6RR(commands.Cog, name="M6RR"):
         self.task = bot.loop.create_task(self.update())
 
     async def update(self):
-        while true:
+        await self.bot.wait_until_ready()
+        while True:
             for guild in self.bot.guilds:
-                await self.update_guild(guild)
+                await self.update_guild(guild.id)
             await asyncio.sleep(120)
 
     # async because it may get more complicated
 
-    async def string_from_snowflake(snowflake):
+    async def string_from_snowflake(self, snowflake):
         return str(snowflake)
 
-    async def load_leaderboard(server_snowflake):
+    async def load_leaderboard(self, server_snowflake):
         async with aiohttp.ClientSession() as session:
             for i in range(1000):  # no servers with >1m members
-                async with session.get(f"https://mee6.xyz/api/plugins/levels/leaderboard{await string_from_snowflake(server_snowflake)}", params={"page": i, "limit": "1000"}) as response:
-                    await print(response.json())
+                async with session.get(f"https://mee6.xyz/api/plugins/levels/leaderboard/{await self.string_from_snowflake(server_snowflake)}", params={"page": i, "limit": "999"}) as response:
+                    resp = (await response.json())["players"]
+                    if not resp:
+                        break
+            else:
+                print("server too big: " + server_snowflake)
 
     async def update_guild(self, guild):
-        await load_leaderboard(guild)
+        await self.load_leaderboard(guild)
 
 
 def setup(bot):
