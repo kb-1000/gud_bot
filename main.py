6fa9b4d5246c2be0b35e7c207990d250075ad1d0
Kaeptm Blaubaer <kaeptmblaubaer1000@gmail.com>
2019-05-29 06:26:57 +0200

    Make bot configuration by environment variables possible

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import discord
 from discord.ext import commands
 import yaml
+import os
 
-with open("config.yaml", "r", encoding="utf-8") as fp:
+with open("config.yaml" if os.path.isfile("config.yaml") else "config.heroku.yaml", "r", encoding="utf-8") as fp:
     config = yaml.unsafe_load(fp)
 
 with open("config.yaml", "w", encoding="utf-8") as fp:
     yaml.dump(config, fp, default_flow_style=False)
 
-bot = commands.Bot(command_prefix="$ ", description="Gud Bot")
+bot = commands.Bot(command_prefix=["$ ", "*", "$"], description="Gud Bot")
 bot.config = config
 bot.load_extension("m6rr")
 bot.load_extension("jishaku")
@@ -21,6 +22,9 @@ async def on_ready():
     await bot.change_presence(activity=discord.Game(name="Gud Bot"))
     print('Successfully set rich presence')
 
-with open("bot_token.txt", "r") as fp:
-    token = fp.read().strip()
+if os.path.isfile("bot_token.txt"):
+    with open("bot_token.txt", "r") as fp:
+        token = fp.read().strip()
+else:
+    token = os.environ["BOT_TOKEN"]
 bot.run(token)
